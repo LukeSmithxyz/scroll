1ce09d0ab8b474c9f2b293080fc2c17b571aff14
Jan Klemkow <j.klemkow@wemelug.de>
2020-04-23 23:42:56 +0200

    fix ptty test program.  don't quit after pos request

--- ptty.c
+++ ptty.c
@@ -131,7 +131,7 @@ main(int argc, char *argv[])
 			/* handle cursor position request */
 			if (strcmp("\033[6n", buf) == 0) {
 				dprintf(mfd, "\033[1;25R", 1, 1);
-				break;
+				continue;
 			}
 
 			if (write(STDOUT_FILENO, buf, n) == -1)
