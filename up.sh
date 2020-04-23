fdb54c317eb9215cbfe063c7086a9cefba334f25
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2020-04-24 00:04:59 +0200

    Fix up test

--- up.sh
+++ up.sh
@@ -5,4 +5,4 @@ set -eu
 jot 50 > tmp.log
 
 (sleep 1; printf "\033[5;2~"; sleep 1; ) \
-	| ktrace -i ./ptty ./scroll ksh -c "tail -fn 50 tmp.log" > out.log
+	| ./ptty ./scroll tail -fn 50 tmp.log > out.log
