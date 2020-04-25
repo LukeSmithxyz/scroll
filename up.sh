fe9ce22d65c5e0effefc1f005c2cb4d860abb13e
Jan Klemkow <j.klemkow@wemelug.de>
2020-04-25 21:15:04 +0200

    replace jot(1) with os independent shell code

--- up.sh
+++ up.sh
@@ -3,7 +3,11 @@
 set -eu
 export POSIXLY_CORRECT=1
 
-jot 50 > tmp.log
+i=1
+while test $i -lt 50; do
+	print $i
+	i=$((i + 1))
+done > tmp.log
 
 (sleep 1; printf "\033[5;2~"; sleep 1; ) \
 	| ./ptty ./scroll tail -fn 50 tmp.log > out.log
