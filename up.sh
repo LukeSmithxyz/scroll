c4b7051d0af4482b594ce065fe77e9f9c0935893
Jan Klemkow <j.klemkow@wemelug.de>
2020-04-26 22:38:40 +0200

    better quoting in scrollup testing shell script

--- up.sh
+++ up.sh
@@ -4,12 +4,12 @@ set -eu
 export POSIXLY_CORRECT=1
 
 i=1
-while test $i -lt 50; do
-	echo $i
+while test "$i" -lt 50; do
+	echo "$i"
 	i=$((i + 1))
 done > tmp.log
 
-(sleep 1; printf "\033[5;2~"; sleep 1; ) \
+(sleep 1; printf '\033[5;2~'; sleep 1; ) \
 	| ./ptty ./scroll tail -fn 50 tmp.log > out.log
 
 cmp out.log up.log
