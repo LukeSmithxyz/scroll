002617abd5bec6a10d938ac5162a278bdd552343
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2020-05-22 09:31:51 +0200

    Add comment

--- config.def.h
+++ config.def.h
@@ -10,6 +10,7 @@ struct rule rules[] = {
 	/* sequence     event        lines */
 	{"\033[5;2~",   SCROLL_UP,   -1},       /* [Shift] + [PageUP] */
 	{"\033[6;2~",   SCROLL_DOWN, -1},       /* [Shift] + [PageDown] */
+	/* mouse binding shadows ^E and ^Y, so it's disabled by default */
 	//{"\031",        SCROLL_UP,    1},       /* mouse wheel up */
 	//{"\005",        SCROLL_DOWN,  1},       /* mouse wheel Down */
 };
